6b83335e2137c83766a9932b1e07abbfeb45b306
Melih Zenciroglu <melih@Melihs-MacBook-Pro.local>
2024-06-18 13:53:44 +0200

    first cfn implementation is done

--- py-starter/py_starter/py_starter_stack.py
+++ py-starter/py_starter/py_starter_stack.py
@@ -2,6 +2,10 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_s3 as s3,
+    Duration,
+    CfnOutput,
+    Fn
 )
 from constructs import Construct
 
@@ -10,10 +14,20 @@ class PyStarterStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        suffix = self.__initialize_suffix()
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PyStarterQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(self, "PyBucket", 
+            bucket_name=f"cool-bucket-{suffix}",               
+            lifecycle_rules=[
+            s3.LifecycleRule(
+                expiration=Duration.days(3)
+            )
+        ])
+
+        CfnOutput(self, "PyBucketName",
+                  value=bucket.bucket_name)
+
+    def __initialize_suffix(self):
+        short_stack_id = Fn.select(2, Fn.split('/', self.stack_id))
+        suffix = Fn.select(4, Fn.split('-', short_stack_id))
+        return suffix
