c17ff2b85328f91da83d28adf2fd6e7a9c338677
Melih Zenciroglu <melih@Melihs-MacBook-Pro.local>
2024-06-18 16:04:55 +0200

    using intrinsic functions-typescript

--- ts-starter/lib/ts-starter-stack.ts
+++ ts-starter/lib/ts-starter-stack.ts
@@ -1,17 +1,33 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_s3 } from 'aws-cdk-lib';
+import { aws_s3, Fn } from 'aws-cdk-lib';
 
 export class TsStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new aws_s3.Bucket(this, 'TsBucket', {
+    const suffix = this.initializeSuffix()
+
+    const bucket = new aws_s3.Bucket(this, 'TsBucket', {
+      bucketName: `cool-bucket-${suffix}`,
       lifecycleRules: [
         {
           expiration: cdk.Duration.days(3)
         }
       ]
     })
+
+    new cdk.CfnOutput(this,
+      'TsBucketName', {
+        value: bucket.bucketName
+      }
+    )
+
+  }
+
+  private initializeSuffix(){
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId))
+    const suffix = Fn.select(4, Fn.split('-', shortStackId))
+    return suffix;
   }
 }
